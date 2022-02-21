b9ef9d79b0d9f69823a9d36f847338a50088f040
Andres <andrescamposb@gmail.com>
2022-02-21 17:46:35 -0300

    Agregando ejercicio interes por cuotas

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-/* PIDO UN NUMERO CON PROMPT Y MUESTRO POR CONSOLA LOS DIVISORES DEL NUMERO INTRODUCIDO*/
+let monto = parseInt(prompt("Ingrese el total del monto a pagar"))
+let cuotas = parseInt(prompt("En cuantas cuotas desea pagar el monto?" + "\n" + "En 1 cuota no tiene interes" + "\n" + "En 3 cuotas tiene 5% de interes" + "\n" + "En 6 cuotas tiene 10% de interes" + "\n" + "En 9 cuotas tiene 15% de interes" + "\n" + "En 12 cuotas tiene 20% de interes"))
+const interes5 = 5*monto/100
+const interes10 = 10*monto/100
+const interes15 = 15*monto/100
+const interes20 = 20*monto/100
+if(cuotas === 1){
+    console.log("Total a pagar: " + monto)
+}else if(cuotas === 3){
+    console.log("Monto a pagar: $" + monto + "\n" + "En " + cuotas + " cuotas" + "\n" + "Interes por cuotas elegidas: $" + interes5 + "\n" + "Total: $" + (monto + interes5))
+}else if(cuotas === 6){
+    console.log("Monto a pagar: $" + monto + "\n" + "En " + cuotas + " cuotas" + "\n" + "Interes por cuotas elegidas: $" + interes10 + "\n" + "Total: $" + (monto + interes10))
+}else if(cuotas === 9){
+    console.log("Monto a pagar: $" + monto + "\n" + "En " + cuotas + " cuotas" + "\n" + "Interes por cuotas elegidas: $" + interes15 + "\n" + "Total: $" + (monto + interes15))
+}else if(cuotas === 12){
+    console.log("Monto a pagar: $" + monto + "\n" + "En " + cuotas + " cuotas" + "\n" + "Interes por cuotas elegidas: $" + interes20 + "\n" + "Total: $" + (monto + interes20))
+}
 
-const numero = parseInt(prompt("Ingrese un numero"))
-for(let i=0; i<=numero; i++){
-    if(numero%i == 0){
-        console.log("Divisor: " + i)
-    }
+while(monto <= 0 || cuotas < 1 || isNaN(monto) || isNaN(cuotas)){
+    monto = parseInt(prompt("Ingrese el total del monto a pagar"))
+    cuotas = parseInt(prompt("En cuantas cuotas desea pagar el monto?" + "\n" + "En 1 cuota no tiene interes" + "\n" + "En 3 cuotas tiene 5% de interes" + "\n" + "En 6 cuotas tiene 10% de interes" + "\n" + "En 9 cuotas tiene 15% de interes" + "\n" + "En 12 cuotas tiene 20% de interes"))
 }
