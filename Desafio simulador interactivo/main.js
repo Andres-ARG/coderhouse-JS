a8f4fe883e49bed580998762d133776d0015ed76
Andres <andrescamposb@gmail.com>
2022-02-27 18:49:33 -0300

    Desafio array

--- Desafio simulador interactivo/main.js
+++ Desafio simulador interactivo/main.js
@@ -1,24 +1,43 @@
-/* Pido un monto a pagar y luego en que cantidad de cuotas quiere pagar. Muestro por consola el monto elegido, el interes y el total a pagar*/
+/* PONGO UNA LISTA DE PRODUCTOS EN UN PROMPT Y EL USUARIO DEBE ESCRIBIR EXACTAMENTE PRODUCTO1, PRODUCTO2 ETC PARA SUMARLO AL CARRITO. LUEGO DOY LA OPCION DE PAGAR EL MONTO TOTAL EN CUOTAS Y MUESTRO LOS RESULTADOS POR CONSOLA*/
 
-let monto = parseInt(prompt("Ingrese el total del monto a pagar"))
-let cuotas = parseInt(prompt("En cuantas cuotas desea pagar el monto?" + "\n" + "En 1 cuota no tiene interes" + "\n" + "En 3 cuotas tiene 5% de interes" + "\n" + "En 6 cuotas tiene 10% de interes" + "\n" + "En 9 cuotas tiene 15% de interes" + "\n" + "En 12 cuotas tiene 20% de interes"))
-const interes5 = 5*monto/100
-const interes10 = 10*monto/100
-const interes15 = 15*monto/100
-const interes20 = 20*monto/100
-if(cuotas === 1){
-    console.log("Total a pagar: " + monto)
-}else if(cuotas === 3){
-    console.log("Monto a pagar: $" + monto + "\n" + "En " + cuotas + " cuotas" + "\n" + "Interes por cuotas elegidas: $" + interes5 + "\n" + "Total: $" + (monto + interes5))
-}else if(cuotas === 6){
-    console.log("Monto a pagar: $" + monto + "\n" + "En " + cuotas + " cuotas" + "\n" + "Interes por cuotas elegidas: $" + interes10 + "\n" + "Total: $" + (monto + interes10))
-}else if(cuotas === 9){
-    console.log("Monto a pagar: $" + monto + "\n" + "En " + cuotas + " cuotas" + "\n" + "Interes por cuotas elegidas: $" + interes15 + "\n" + "Total: $" + (monto + interes15))
-}else if(cuotas === 12){
-    console.log("Monto a pagar: $" + monto + "\n" + "En " + cuotas + " cuotas" + "\n" + "Interes por cuotas elegidas: $" + interes20 + "\n" + "Total: $" + (monto + interes20))
+const carrito = []
+const producto1 = 5000
+const producto2 = 3000
+const producto3 = 2000
+const producto4 = 7000
+let total = 0
+let listaProductos = prompt("LISTA DE PRODUCTOS DISPONIBLES:" + "\n" + "producto1" + "\n" + "producto2" + "\n" + "producto3" + "\n" + "producto4" + "\n" + "ESCRIBA EL PRODUCTO QUE DESEA AGREGAR AL CARRITO")
+carrito.push(listaProductos)
+while(confirm("Desea agregar algo mas al carrito?")){
+    listaProductos = prompt("LISTA DE PRODUCTOS DISPONIBLES:" + "\n" + "producto1" + "\n" + "producto2" + "\n" + "producto3" + "\n" + "producto4" + "\n" + "ESCRIBA EL PRODUCTO QUE DESEA AGREGAR AL CARRITO")
+    carrito.push(listaProductos)
 }
-
-while(monto <= 0 || cuotas < 1 || isNaN(monto) || isNaN(cuotas)){
-    monto = parseInt(prompt("Ingrese el total del monto a pagar"))
-    cuotas = parseInt(prompt("En cuantas cuotas desea pagar el monto?" + "\n" + "En 1 cuota no tiene interes" + "\n" + "En 3 cuotas tiene 5% de interes" + "\n" + "En 6 cuotas tiene 10% de interes" + "\n" + "En 9 cuotas tiene 15% de interes" + "\n" + "En 12 cuotas tiene 20% de interes"))
+for(i=0; i<carrito.length; i++){
+    if(carrito[i] == "producto1"){
+        total += producto1
+    }else if(carrito[i] == "producto2"){
+        total += producto2
+    }else if(carrito[i] == "producto3"){
+        total += producto3
+    }else if(carrito[i] == "producto4"){
+        total += producto4
+    }else{
+        console.log("Introduce exactamente 'producto1' 'producto2' etc")
+    }
+}
+let cuotas = parseInt(prompt("En cuantas cuotas desea pagar el monto?" + "\n" + "En 1 cuota no tiene interes" + "\n" + "En 3 cuotas tiene 5% de interes" + "\n" + "En 6 cuotas tiene 10% de interes" + "\n" + "En 9 cuotas tiene 15% de interes" + "\n" + "En 12 cuotas tiene 20% de interes"))
+const interes5 = 5*total/100
+const interes10 = 10*total/100
+const interes15 = 15*total/100
+const interes20 = 20*total/100
+if(cuotas === 1){
+    console.log("Total a pagar: " + total)
+}else if(cuotas === 3){
+    console.log("Monto a pagar: $" + total + "\n" + "En " + cuotas + " cuotas" + "\n" + "Interes por cuotas elegidas: $" + interes5 + "\n" + "Total: $" + (total + interes5))
+}else if(cuotas === 6){
+    console.log("Monto a pagar: $" + total + "\n" + "En " + cuotas + " cuotas" + "\n" + "Interes por cuotas elegidas: $" + interes10 + "\n" + "Total: $" + (total + interes10))
+}else if(cuotas === 9){
+    console.log("Monto a pagar: $" + total + "\n" + "En " + cuotas + " cuotas" + "\n" + "Interes por cuotas elegidas: $" + interes15 + "\n" + "Total: $" + (total + interes15))
+}else if(cuotas === 12){
+    console.log("Monto a pagar: $" + total + "\n" + "En " + cuotas + " cuotas" + "\n" + "Interes por cuotas elegidas: $" + interes20 + "\n" + "Total: $" + (total + interes20))
 }
