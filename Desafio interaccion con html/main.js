3fe48ea9f2b3a5c51d0a5979afc4228558a7e9ef
Andres <andrescamposb@gmail.com>
2022-03-11 21:12:40 -0300

    agregando proyecto incluyendo eventos y demas

--- Desafio interaccion con html/main.js
+++ Desafio interaccion con html/main.js
@@ -1,85 +1,102 @@
-const libros = [
-    {
-        nombre: "La invención de Morel",
-        autor: "Norah Borges",
-        publicacion: 1940,
-        idioma: "Español",
-    },
-    {
-        nombre: "El túnel",
-        autor: "Ernesto Sábato",
-        publicacion: 1948,
-        idioma: "Español",
-    },
-    {
-        nombre: "Sobre héroes y tumbas",
-        autor: "Ernesto Sábato",
-        publicacion: 1961,
-        idioma: "Español",
-    }
-]
-const nombreBienvenida = prompt("Hola, cual es tu nombre?")
-const contenedor = document.createElement("div")
-contenedor.style.display = "block"
-contenedor.style.textAlign = "center"
-contenedor.style.padding = "20px"
-contenedor.style.fontWeight = "bold"
-contenedor.style.fontSize = "20px"
-contenedor.innerHTML = `Bienvenido/a ${nombreBienvenida}`
-const header = document.querySelector(".header")
-header.appendChild(contenedor)
+//INGRESAR EN EL INPUT EL EJERCICIO A AGREGAR A LA LISTA DE EJERCICIOS, SE AGREGARA ABAJO EL NUEVO ITEM.
+//SE PODRA TILDAR EL ITEM PARA MARCAR COMO REALIZADO Y TAMBIEN BORRARLO DE LA LISTA
+//USE LOCALSTORAGE
 
-const opciones = parseInt(prompt(
-    `Elija la opcion deseada:
-    1- Listar libros de la biblioteca
-    2- Agregar libro a la biblioteca
-    3- Buscar libro de la biblioteca
-    4- Filtrar
-    5- Salir
-    `
-    ))
-    class libro{
-        constructor(nombre, autor, publicacion, idioma){
-            this.nombre = prompt("Ingrese el nombre del libro")
-            this.autor = prompt("Ingrese el autor del libro")
-            this.publicacion = parseInt(prompt("Ingrese el anio de publicacion del libro"))
-            this.idoma = prompt("Ingrese el idioma del libro")
-        }
+// VOY A SEPARAR EL CODIGO EN TRES PARTES : VARIABLES, FUNCIONES, EVENTOS
+
+// VARIABLES
+
+const formulario = document.querySelector("#form")
+const listaEjercicios = document.querySelector("#listaEjercicios")
+let arrayEjercicios = []
+
+//FUNCIONES
+
+const crearEjercicio = (ejercicio) => {
+    let itemEjercicio = {
+        ejercicio: ejercicio,
+        estado: false
     }
-        switch(opciones){
-            case 1:{
-            listarLibros()
-            break
-        }case 2:{
-            const nuevoLibro = new libro
-            libros.push(nuevoLibro)
-            break
-        }case 3:{
-            buscarLibro()
-            break
-        }case 4:{
-            filtrar()
-            break
-        }
-    }
-function listarLibros(){
-    libros.forEach(libro => {
-        console.log(libro)
-    });
+    arrayEjercicios.push(itemEjercicio)
 }
-function buscarLibro(){
-    const filtro = prompt("Ingrese el nombre del libro a buscar")
-    const busqueda = libros.find((libro) => libro.nombre === filtro)
-    console.log(busqueda)
-    if(busqueda != undefined){
-        console.log("El libro si existe")
+
+let guardar = () => {
+    localStorage.setItem("rutina", JSON.stringify(arrayEjercicios))
+    pintar()
+}
+
+let pintar = () => {
+    listaEjercicios.innerHTML = ""
+    arrayEjercicios = JSON.parse(localStorage.getItem("rutina"))
+    if(arrayEjercicios === null){
+        arrayEjercicios = []
     }else{
-        console.log("El libro no existe en la biblioteca")
+        arrayEjercicios.forEach(element => {
+            
+            if(element.estado){
+                listaEjercicios.innerHTML += `
+                <div class="alert alert-success d-flex align-items-center justify-content-between" role="alert">
+                <i class="material-icons">accessibility</i>
+                <b>${element.ejercicio}</b> - ${element.estado}
+                <span>
+                    <i class="material-icons">done</i>
+                    <i class="material-icons">delete</i>
+                </span>
+                </div>
+                `
+            }else {
+                listaEjercicios.innerHTML += `
+                <div class="alert alert-danger d-flex align-items-center justify-content-between" role="alert">
+                <i class="material-icons">accessibility</i>
+                <b>${element.ejercicio}</b> - ${element.estado}
+                <span>
+                    <i class="material-icons">done</i>
+                    <i class="material-icons">delete</i>
+                </span>
+                </div>
+                `
+            }
+        })
     }
 }
-function filtrar(){
-    /* SOLO VA A FILTRAR LOS LIBROS CON ANIOS DE PUBLICACION MAYORES A 1950*/
-    
-    const filtrado = libros.filter((libro) => libro.publicacion > 1950)
-    console.log(filtrado)
+
+let eliminar = (ejercicio) => {
+    let indexArray;
+    arrayEjercicios.forEach((elemento, index) => {
+        if(elemento.ejercicio === ejercicio){
+            indexArray = index
+        }
+    })
+    arrayEjercicios.splice(indexArray,1)
+    guardar()
 }
+
+let editar = (ejercicio) => {
+    let indexArray = arrayEjercicios.findIndex((elemento) => elemento.ejercicio === ejercicio)
+    arrayEjercicios[indexArray].estado = true
+    guardar()
+}
+
+//EVENTOS
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let ejercicio = document.querySelector("#ejercicio").value
+    crearEjercicio(ejercicio)
+    guardar()
+    formulario.reset()
+})
+
+document.addEventListener("DOMContentLoaded", pintar)
+listaEjercicios.addEventListener("click", (e) => {
+    e.preventDefault()
+    if(e.target.innerHTML === "done" || e.target.innerHTML === "delete"){
+        let texto = e.path[2].childNodes[3].innerHTML
+        if(e.target.innerHTML === "delete"){
+            eliminar(texto)
+        }
+        if(e.target.innerHTML === "done"){
+            editar(texto)
+        }
+    }
+})
